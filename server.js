69a80dd8ec92b45ae0f8268fec1fdd5d043815cd
amardeep0070 <amardeep007-@gmail.com>
2016-09-16 14:08:21 -0400

    commit 2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', (process.env.PORT || 5000));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
@@ -15,4 +16,6 @@ require ("./test/app.js")(app);
 
 //app.listen(port, ipaddress);
 console.log(process.env);
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
